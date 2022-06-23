579f8f42b4ffbd3f4605d1eecbedcadcea5d1d3a
evgeny <zhek12322@gmail.com>
2022-06-23 13:23:11 +0300

    step10.10

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-from brain_games.games.progression_games import calculate
+from brain_games.games.progression_games import caclulate
 from brain_games.games.logic_games import tune
 
 
 def main():
-    tune(calculate)
+    tune(caclulate)
 
 
 if __name__ == '__main__':
