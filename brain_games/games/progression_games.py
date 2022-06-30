f80bbd9e5d5338cfa9c8f66fba12513f60080122
evgeny <zhek12322@gmail.com>
2022-06-30 17:35:25 +0300

    step10.14

--- brain_games/games/progression_games.py
+++ brain_games/games/progression_games.py
@@ -4,18 +4,18 @@ MIN_NUMBER = 1
 MAX_NUMBER = 100
 MIN_STEP = 1
 MAX_STEP = 12
-MIN_NUMBER_STEPS = 5
-MAX_NUMBER_STEPS = 10
+MIN_COUNT_STEPS = 5
+MAX_COUNT_STEPS = 10
 FIRST_ELEMENT_PROGRESSION = 0
 
 
 def generate_round_game():
     start_progression = randint(MIN_NUMBER, MAX_NUMBER)
     step = randint(MIN_STEP, MAX_STEP)
-    number_steps = randint(MIN_NUMBER_STEPS, MAX_NUMBER_STEPS)
-    end_progression = start_progression + step * number_steps
+    number_count = randint(MIN_COUNT_STEPS, MAX_COUNT_STEPS)
+    end_progression = start_progression + step * number_count
     progression = (list(range(start_progression, end_progression, step)))
-    random_number = (randint(FIRST_ELEMENT_PROGRESSION, number_steps - 1))
+    random_number = (randint(FIRST_ELEMENT_PROGRESSION, number_count - 1))
     answer = progression[random_number]
     progression[random_number] = '..'
     question = ''
